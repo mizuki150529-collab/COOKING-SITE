4809e087de706a1faf051152d880ae256985df0a
mizuki150529-collab <mizuki150529@gmail.com>
2026-02-15 21:24:53 +0900

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -49,15 +49,6 @@ ActiveRecord::Schema[7.2].define(version: 2026_01_17_125303) do
     t.index ["user_id"], name: "index_cooking_logs_on_user_id"
   end
 
-  create_table "post_tag_relations", force: :cascade do |t|
-    t.integer "post_id", null: false
-    t.integer "tag_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["post_id"], name: "index_post_tag_relations_on_post_id"
-    t.index ["tag_id"], name: "index_post_tag_relations_on_tag_id"
-  end
-
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "ingredients"
@@ -67,12 +58,6 @@ ActiveRecord::Schema[7.2].define(version: 2026_01_17_125303) do
     t.integer "difficulty"
     t.string "cuisine_type"
     t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "tags", force: :cascade do |t|
-    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -95,6 +80,4 @@ ActiveRecord::Schema[7.2].define(version: 2026_01_17_125303) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "cooking_logs", "posts"
   add_foreign_key "cooking_logs", "users"
-  add_foreign_key "post_tag_relations", "posts"
-  add_foreign_key "post_tag_relations", "tags"
 end
