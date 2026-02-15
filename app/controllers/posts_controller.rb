b3eaf369a36d2a33d0cd846e13ce57909079132d
mizuki150529-collab <mizuki150529@gmail.com>
2026-02-15 20:56:03 +0900

    first commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,10 +20,10 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(post_params)
-    @post.user_id = current_user.id
+    post = Post.new(post_params)
+    post.user_id = current_user.id
 
-    if @post.save
+    if post.save
       redirect_to posts_path, notice: "投稿しました"
     else
       render :new, status: :unprocessable_entity
